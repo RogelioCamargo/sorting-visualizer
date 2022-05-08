bb85e303586783431eaaf3219fa33978742b4cdb
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-08 12:37:07 -0700

    Create utility functions to get a variable length array filled with random numbers between a range

---
+++ src/SortingVisualizer/util.ts
@@ -0,0 +1,13 @@
+const getNumberFromRange = (min: number, max: number): number => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+};
+
+const getArrayFromRange = (amount: number, min: number, max: number): Array<number> => {
+	const array = [];
+	for (let i = 0; i < amount; i++) array.push(getNumberFromRange(min, max));
+	return array;
+};
+
+export { getNumberFromRange, getArrayFromRange };
