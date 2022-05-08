57588a8580899d4c67de53aabf23ce67eb938231
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-08 13:03:45 -0700

    Display array and add a reset button

--- src/SortingVisualizer/SortingVisualizer.tsx
+++ src/SortingVisualizer/SortingVisualizer.tsx
@@ -5,15 +5,25 @@ const SortingVisualizer = () => {
 	const [array, setArray] = useState<number[]>([]);
 
 	useEffect(() => {
-		setArray(getArrayFromRange(100, 10, 500));
+		setArray(getArrayFromRange(100, 10, 300));
 	}, []);
+
+	const reset = () => {
+		setArray(getArrayFromRange(100, 10, 300));
+	};
 
 	return (
 		<div>
-			<h1>Sorting Visualizer</h1>
-			{array.map((value) => (
-				<p>{value}</p>
-			))}
+			<h1 className="font-bold">Sorting Visualizer</h1>
+			<button className="bg-indigo-600 text-white w-32" onClick={reset}>Reset</button>
+			<div className="flex justify-center">
+				{array.map((value) => (
+					<div
+						className="w-2 bg-indigo-600 m-px"
+						style={{ height: `${value}px` }}
+					/>
+				))}
+			</div>
 		</div>
 	);
 };
