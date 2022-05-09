ad68b61b20cc49c89975b1a1450948f11f868f80
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-09 12:33:37 -0700

    Update mergeSort to return an array of animations instead of just the sorted array

--- src/algorithms/mergeSort.ts
+++ src/algorithms/mergeSort.ts
@@ -1,21 +1,24 @@
-const mergeSort = (array: Array<number>) => {
+const getMergeSortAnimations = (array: Array<number>): Array<number[]> => {
+	if (array.length <= 1) return [[]]; 
+	const animations: Array<number[]> = []; 
 	const duplicate = array.slice();
-	mergeSortHelper(array, 0, array.length - 1, duplicate);
-	return array;
+	mergeSortHelper(array, 0, array.length - 1, duplicate, animations);
+	return animations;
 };
 
 const mergeSortHelper = (
 	mainArray: Array<number>,
 	startIndex: number,
 	endIndex: number,
-	duplicateArray: Array<number>
+	duplicateArray: Array<number>, 
+	animations: Array<number[]>
 ) => {
 	if (startIndex === endIndex) return;
 
 	const middleIndex = Math.floor((startIndex + endIndex) / 2);
-	mergeSortHelper(duplicateArray, startIndex, middleIndex, mainArray);
-	mergeSortHelper(duplicateArray, middleIndex + 1, endIndex, mainArray);
-	doMerge(mainArray, startIndex, middleIndex, endIndex, duplicateArray);
+	mergeSortHelper(duplicateArray, startIndex, middleIndex, mainArray, animations);
+	mergeSortHelper(duplicateArray, middleIndex + 1, endIndex, mainArray, animations);
+	doMerge(mainArray, startIndex, middleIndex, endIndex, duplicateArray, animations);
 };
 
 const doMerge = (
@@ -23,20 +26,38 @@ const doMerge = (
 	startIndex: number,
 	middleIndex: number,
 	endIndex: number,
-	duplicateArray: Array<number>
+	duplicateArray: Array<number>, 
+	animations: Array<number[]>
 ) => {
 	let k = startIndex;
 	let i = startIndex;
 	let j = middleIndex + 1;
 
 	while (i <= middleIndex && j <= endIndex) {
-		if (duplicateArray[i] < duplicateArray[j])
+		animations.push([i, j]); 
+		animations.push([i, j]); 
+		if (duplicateArray[i] < duplicateArray[j]) {
+			animations.push([k, duplicateArray[i]]); 
 			mainArray[k++] = duplicateArray[i++];
-		else mainArray[k++] = duplicateArray[j++];
+		}
+		else {
+			animations.push([k, duplicateArray[j]]); 
+			mainArray[k++] = duplicateArray[j++];
+		}
 	}
 
-	while (i <= middleIndex) mainArray[k++] = duplicateArray[i++];
-	while (j <= endIndex) mainArray[k++] = duplicateArray[j++];
+	while (i <= middleIndex) {
+		animations.push([i, i]); 
+		animations.push([i, i]); 
+		animations.push([k, duplicateArray[i]])
+		mainArray[k++] = duplicateArray[i++];
+	}
+	while (j <= endIndex) {
+		animations.push([j, j]); 
+		animations.push([j, j]); 
+		animations.push([k, duplicateArray[j]]); 
+		mainArray[k++] = duplicateArray[j++];
+	}
 };
 
-export default mergeSort;
+export default getMergeSortAnimations;
